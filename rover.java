df05e1b36d2978a01364ad6385938c60631837aa
dev-Anthonym <dev.anthony.m@gmail.com>
2026-08-31 21:17:59 -0300

    Adicionando os códigos

---
+++ rover.java
@@ -0,0 +1,13 @@
+// Rover.JAVA
+public class Rover {
+
+    public static void inicilizarRover() {
+        System.out.println("Sistemas do Rover iniciados!");
+        System.out.println("Painéis solares: OK");
+        System.out.println("Nível de bateria: 100%");
+    }
+
+    public static void main(String[] args){
+        inicializarRover();
+    }
+}
